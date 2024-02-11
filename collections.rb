2d404366fa8ab23e1165ff793f88c0c22e9cd237
fwrw <heretostayost@gmail.com>
2024-02-11 03:42:42 -0300

    power

---
+++ collections.rb
@@ -0,0 +1,25 @@
+#Arrays
+
+arr = ['one', 2.0, true]
+
+arr.push(4)
+arr << 5
+arr.insert(5, "six")
+
+size = arr.count
+print(size)
+size = arr.length
+print(size)
+size = arr.size
+print(size)
+
+
+#Hashes
+
+person = {
+  "name" => 'john',
+  age: 21
+}
+
+person["name"] = 'klyde'
+print(person)
